da5954ccf6f0d7e7aff17185e1e3a058053be487
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-07-05 16:50:13 -0700

    update

---
+++ cardsagainstcomputers.js
@@ -0,0 +1,38 @@
+function play1(){
+    document.getElementById("output").innerHTML = "I will play card " +Math.floor(Math.random() * 7+1);;
+    }
+
+function play2(){
+    document.getElementById("output").innerHTML = "I will play cards " +Math.floor(Math.random() * 7+1) +"and"+Math.floor(Math.random() * 6+1);
+    }
+
+function play3(){
+    document.getElementById("output").innerHTML = "I will play cards " +Math.floor(Math.random() * 7+1) +","+Math.floor(Math.random() * 6+1)+"and"+Math.floor(Math.random() * 5+1);
+    }
+
+function judge(){
+    var players = document.getElementById("playernumberinput").value;
+    document.getElementById("output").innerHTML = "I pick " +Math.floor(Math.random() * players+1);;
+    }
+
+
+document.getElementById("playernumberinput").value =
+  localStorage["numberofplayers"] || ""; // 
+
+
+function save1(){
+  document.getElementById("playernumber").innerHTML = document.getElementById("playernumberinput").value;
+  localStorage.setItem("numberofplayers", document.getElementById("playernumberinput").value);
+}
+
+function load(){
+    document.getElementById("playernumberinput").value = localStorage["numberofplayers"];
+    document.getElementById("playernumber").innerHTML = localStorage["numberofplayers"];
+}
+
+setInterval(function() {
+
+    localStorage["numberofplayers"] = document.getElementById("playernumberinput").value; // 
+    localStorage["numberofplayers"] = document.getElementById("playernumber").innerHTML; // 
+
+  }, 1000);
